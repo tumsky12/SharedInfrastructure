91e83db2c4ec61324c55e87b9c7a9ee9a67c9575
Arkius <arkiustum@gmail.com>
2023-09-23 17:13:52 +0100

    added core CoreInfrastructure

---
+++ CoreInfrastructure/Terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_location" {
+  default     = "uksouth"
+  description = "Location of the resource group."
+}
+
+variable "resource_prefix" {
+  default     = "core"
+  description = "Prefix of the resource."
+}
+
+variable "resource_suffix" {
+  default     = "prod"
+  description = "Prefix of the resource."
+}
