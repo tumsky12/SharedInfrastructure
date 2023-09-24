c2c08091dc6319e342379fc2d0d315e995b277cb
Arkius <arkiustum@gmail.com>
2023-09-24 14:15:34 +0100

    more formatting

--- VirtualMachineInfrastructure/Terraform/locals.tf
+++ VirtualMachineInfrastructure/Terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  shared_resource_group_name   = "${var.resource_prefix}-rg-${var.resource_environment}"
+  shared_resource_group_name    = "${var.resource_prefix}-rg-${var.resource_environment}"
   shared_network_interface_name = "${var.resource_prefix}-nic-${var.resource_environment}"
 }
