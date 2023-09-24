b69ff45d3f2d5bde517e673b4fdac1dd59cb7c45
Arkius <arkiustum@gmail.com>
2023-09-24 12:18:02 +0100

    added networking to core

---
+++ CoreInfrastructure/Terraform/virtualNetwork.tf
@@ -0,0 +1,25 @@
+resource "azurerm_virtual_network" "vn" {
+  name                = "${var.resource_prefix}-net-${var.resource_environment}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sn" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.resource_prefix}-nic-${var.resource_environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
