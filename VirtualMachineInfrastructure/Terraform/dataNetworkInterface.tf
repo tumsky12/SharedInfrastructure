e13de56ed1c0cd0a7d8ccf0af9d020b829822df6
Arkius <arkiustum@gmail.com>
2023-09-24 14:08:37 +0100

    terraform fmt

--- VirtualMachineInfrastructure/Terraform/dataNetworkInterface.tf
+++ VirtualMachineInfrastructure/Terraform/dataNetworkInterface.tf
@@ -1,4 +1,4 @@
 data "azurerm_network_interface" "nic" {
-  name = local.shared_resource_group_name
+  name                = local.shared_resource_group_name
   resource_group_name = local.sharednetwork_interface_name
 }
