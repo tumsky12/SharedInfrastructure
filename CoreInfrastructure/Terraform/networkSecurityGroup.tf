27512a584ad83ce2d112866ca1647860b3647e3c
Arkius <arkiustum@gmail.com>
2023-09-24 19:36:51 +0100

    open network group rdp port

--- CoreInfrastructure/Terraform/networkSecurityGroup.tf
+++ CoreInfrastructure/Terraform/networkSecurityGroup.tf
@@ -4,16 +4,16 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# resource "azurerm_network_security_rule" "example" {
-#   name                        = "test123"
-#   priority                    = 100
-#   direction                   = "Outbound"
-#   access                      = "Allow"
-#   protocol                    = "Tcp"
-#   source_port_range           = "*"
-#   destination_port_range      = "*"
-#   source_address_prefix       = "*"
-#   destination_address_prefix  = "*"
-#   resource_group_name         = azurerm_resource_group.example.name
-#   network_security_group_name = azurerm_network_security_group.example.name
-# }
+resource "azurerm_network_security_rule" "nsr" { #Todo: dont do this! vpn/ private connection instead
+  name                        = "${var.resource_prefix}-nsr-rdp-${var.resource_environment}"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
