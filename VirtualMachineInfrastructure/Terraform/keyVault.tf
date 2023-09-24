280af3f8bfecc2cb26bac180796f82dcbda4d0a5
Arkius <arkiustum@gmail.com>
2023-09-24 20:33:08 +0100

    add vault admin role assignment to client

--- VirtualMachineInfrastructure/Terraform/keyVault.tf
+++ VirtualMachineInfrastructure/Terraform/keyVault.tf
@@ -11,11 +11,16 @@ resource "azurerm_key_vault" "kv" {
   sku_name = "standard"
 }
 
+resource "azurerm_role_assignment" "ra_kv_admin" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 resource "azurerm_key_vault_secret" "kvs" {
   name         = "admin-password"
   value        = random_password.vm_password.result
   key_vault_id = azurerm_key_vault.kv.id
 
-  #depends_on = [ azurerm_key_vault_access_policy.kvap ]
-  #depends_on = [azurerm_role_assignment.ra_kv_admin]
+  depends_on = [azurerm_role_assignment.ra_kv_admin]
 }
