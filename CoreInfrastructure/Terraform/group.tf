a0ff1a0e6ef51eabdc2fcababb6e901e8eeb6cbc
Arkius <arkiustum@gmail.com>
2023-09-26 22:22:49 +0100

    correction to azurerm stuffs

--- CoreInfrastructure/Terraform/group.tf
+++ CoreInfrastructure/Terraform/group.tf
@@ -1,6 +1,6 @@
 resource "azuread_group" "g-owner" {
   display_name     = "owner-${var.resource_environment}"
-  owners           = [data.azuread_client_config.current.object_id]
+  owners           = [data.azurerm_client_config.current.object_id]
   security_enabled = true
 
   members = [
