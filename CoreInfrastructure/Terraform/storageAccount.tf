4576536ce1e28d16edea68b3dbd544567cf5f037
Arkius <arkiustum@gmail.com>
2023-10-06 21:37:37 +0100

    further corrections to the new storage account addition to core

--- CoreInfrastructure/Terraform/storageAccount.tf
+++ CoreInfrastructure/Terraform/storageAccount.tf
@@ -1,8 +1,8 @@
 module "sa" {
   source                  = "../../TerraformModules/StorageAccount"
-  resource_group_location = data.azurerm_resource_group.rg.location
+  resource_group_location = azurerm_resource_group.rg.location
   resource_prefix         = var.resource_prefix
-  resource_group_name     = data.azurerm_resource_group.rg.name
+  resource_group_name     = azurerm_resource_group.rg.name
   resource_name           = "sa"
   resource_environment    = var.resource_environment
 }
