1c97763cdd7a56884fe2c5be901ae7295ccafe22
Arkius <arkiustum@gmail.com>
2023-10-06 21:30:55 +0100

    corrections to idiotic mistake in new core storage account refing kv module

--- CoreInfrastructure/Terraform/storageAccount.tf
+++ CoreInfrastructure/Terraform/storageAccount.tf
@@ -1,5 +1,5 @@
-module "kv" {
-  source                  = "../../TerraformModules/KeyVault"
+module "sa" {
+  source                  = "../../TerraformModules/StorageAccount"
   resource_group_location = data.azurerm_resource_group.rg.location
   resource_prefix         = var.resource_prefix
   resource_group_name     = data.azurerm_resource_group.rg.name
