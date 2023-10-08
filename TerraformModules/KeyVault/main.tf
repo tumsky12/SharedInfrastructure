69ee78865ecb4dbe9562e9b835e00bcc2683c572
Arkius <arkiustum@gmail.com>
2023-10-08 19:14:57 +0100

    added certificate roles to kv

--- TerraformModules/KeyVault/main.tf
+++ TerraformModules/KeyVault/main.tf
@@ -11,14 +11,26 @@ resource "azurerm_key_vault" "kv" {
   sku_name = "standard"
 }
 
-resource "azurerm_role_assignment" "ra_kv_admin" {
+resource "azurerm_role_assignment" "ra_kv_admin_current" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
 }
 
-resource "azurerm_role_assignment" "ra_kv_user" {
+resource "azurerm_role_assignment" "ra_kv_admin_owners" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Administrator"
+  principal_id         = module.constants.owners_group_id
+}
+
+resource "azurerm_role_assignment" "ra_kv_certofficer_current" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Certificates Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "ra_kv_certofficer_owner" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Certificates Officer"
   principal_id         = module.constants.owners_group_id
 }
