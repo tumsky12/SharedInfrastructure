1b644df5958f172f0d01cd740f436a986f1e5fcf
Arkius <arkiustum@gmail.com>
2023-10-20 00:11:32 +0100

    added primary access key to storage account module

--- TerraformModules/StorageAccount/outputs.tf
+++ TerraformModules/StorageAccount/outputs.tf
@@ -5,3 +5,7 @@ output "storage_account_id" {
 output "storage_account_name" {
   value = azurerm_storage_account.sa.name
 }
+
+output "primary_access_key" {
+  value = azurerm_storage_account.sa.primary_access_key
+}
