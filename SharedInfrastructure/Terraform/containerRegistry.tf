26097b2038e9778194a8d58aa7cedb76c9cc0269
tumsky12 <atumtum12@gmail.com>
2024-02-19 22:27:18 +0000

    sdfg

--- SharedInfrastructure/Terraform/containerRegistry.tf
+++ SharedInfrastructure/Terraform/containerRegistry.tf
@@ -6,7 +6,7 @@ resource "random_string" "random" {
 }
 
 resource "azurerm_container_registry" "cr" {
-  name                = "crShared${random.result}"
+  name                = "crShared${random_string.random.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
