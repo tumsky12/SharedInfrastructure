36cfc2b43b7b8b535ed978d093680c4fb4e507c5
tumsky12 <atumtum12@gmail.com>
2024-02-20 22:58:22 +0000

    added keyvault siffix

--- CoreInfrastructure/Terraform/keyVault.tf
+++ CoreInfrastructure/Terraform/keyVault.tf
@@ -1,3 +1,8 @@
+resource "random_integer" "random" {
+  min = 10000
+  max = 99999
+}
+
 module "kv" {
   source                  = "../../../CoreTerraformModules/Modules/KeyVault"
   resource_group_location = azurerm_resource_group.rg.location
@@ -5,4 +10,5 @@ module "kv" {
   resource_group_name     = azurerm_resource_group.rg.name
   resource_name           = "kv"
   resource_environment    = var.resource_environment
+  key_vault_suffix        = random_integer.random.result
 }
