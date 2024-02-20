65ab7bd661b12d20c0c1af16a23ec2aa3e522721
tumsky12 <atumtum12@gmail.com>
2024-02-20 22:37:06 +0000

    Correction to core infra kv rg refs

--- CoreInfrastructure/Terraform/keyVault.tf
+++ CoreInfrastructure/Terraform/keyVault.tf
@@ -1,8 +1,8 @@
 module "kv" {
   source                  = "../../../CoreTerraformModules/Modules/KeyVault"
-  resource_group_location = data.azurerm_resource_group.rg.location
+  resource_group_location = azurerm_resource_group.rg.location
   resource_prefix         = var.resource_prefix
-  resource_group_name     = data.azurerm_resource_group.rg.name
-  resource_name           = "kv-vm"
+  resource_group_name     = azurerm_resource_group.rg.name
+  resource_name           = "kv"
   resource_environment    = var.resource_environment
 }
